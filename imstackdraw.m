76e42da68bd4e1099ae3a5e7671214a95bc6d8f1
MooGu Z <moogu@live.cn>
2017-06-12 15:30:42 -0400

    fix the layout of 'imstackdraw'

--- imstackdraw.m
+++ imstackdraw.m
@@ -3,7 +3,15 @@ function canvas = imstackdraw(imstack, varargin)
 % NOTE: only deal with gray image currently
 conf = Config(varargin);
 % get fundamental information
-[height, width, n] = size(imstack);
+if nndims(imstack) >= 4
+    % Color Images
+    iscolor = true;
+    assert(size(imstack, 3) == 3, 'ILLEGAL COLOR IMAGE');
+    [height, width, ~, n] = size(imstack);
+else
+    iscolor = false;
+    [height, width, n] = size(imstack);
+end
 if conf.exist('arrange')
     arrangement = conf.pop('arrange');
     nrow = arrangement(1);
@@ -14,16 +22,28 @@ end
 border = conf.pop('border', 3);
 background = conf.pop('background', 0.5);
 % create canvas
-canvas = ones(border + ([height, width] + border) .* [nrow, ncol]) * background;
+if iscolor
+    canvas = background * ones( ...
+        [border + ([height, width] + border) .* [nrow, ncol], 3]);
+else
+    canvas = background * ones( ...
+        border + ([height, width] + border) .* [nrow, ncol]);
+end
 % copy each image to canvas
-ycord = size(canvas, 1) - border + 1;
+ycord = border;
 for r = 1 : nrow
     xcord = border;
     for c = 1 : ncol
         index = (r - 1)* ncol + c;
         if index > n, return; end
-        canvas(ycord - (height : -1 : 1), xcord + (1 : width)) = imstack(:, :, index);
+        if iscolor
+            canvas(ycord + (1 : height), xcord + (1 : width), :) ...
+                = imstack(:, :, :, index);
+        else
+            canvas(ycord + (1 : height), xcord + (1 : width)) ...
+                = imstack(:, :, index);
+        end
         xcord = xcord + width + border;
     end
-    ycord = ycord - height - border;
+    ycord = ycord + height + border;
 end
